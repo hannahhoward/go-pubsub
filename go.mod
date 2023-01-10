63a40f927dea9b129bddbb633cd1c56dc6d8f8ef
hannahhoward <hannah@hannahhoward.net>
2023-01-10 15:27:03 -0800

    feat(ready): add ready manager utility

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module github.com/hannahhoward/go-pubsub
 
-go 1.14
+go 1.18
+
+require github.com/stretchr/testify v1.5.1
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
-	github.com/stretchr/testify v1.5.1
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.4 // indirect
 )
---
+++ ready/ready_test.go
@@ -0,0 +1,64 @@
+package ready_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hannahhoward/go-pubsub/ready"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadyManager(t *testing.T) {
+	rm := ready.NewReadyManager()
+
+	// Verify that AwaitReady blocks until FireReady is called
+	awaitReadyComplete := make(chan error)
+	go func() {
+		err := rm.AwaitReady()
+		awaitReadyComplete <- err
+	}()
+	select {
+	case <-awaitReadyComplete:
+		require.Fail(t, "AwaitReady should block until FireReady is called")
+	default:
+	}
+
+	// Verify that OnReady is called with the error supplied to FireReady
+	errs := make(chan error)
+	rm.OnReady(func(err error) {
+		errs <- err
+	})
+
+	someErr := fmt.Errorf("some err")
+	go rm.FireReady(someErr)
+
+	select {
+	case err := <-errs:
+		require.Equal(t, someErr, err)
+	}
+
+	select {
+	case <-time.After(100 * time.Millisecond):
+		require.Fail(t, "AwaitReady should complete when FireReady is called")
+	case err := <-awaitReadyComplete:
+		require.Equal(t, someErr, err)
+	}
+
+	// Verify that calling OnReady after FireReady has been called results
+	// in the callback being called immediately
+	errs2 := make(chan error)
+	rm.OnReady(func(err error) {
+		errs2 <- err
+	})
+
+	select {
+	case err := <-errs2:
+		require.Equal(t, someErr, err)
+	}
+
+	// Verify that calling AwaitReady after FireReady has been called returns
+	// immediately
+	rm.AwaitReady()
+	t.Log("AwaitReady complete")
+}
